cb62ec6382a4c04d4fc5f74010d1409d9ff0323c
robione-nr <nrobidoux@hotmail.com>
2020-07-31 21:50:14 -0500

    Get creation time for V1.

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Erlang - UUID Server
 A simple UUID generator for use in applications.
 
-This project implements most UUID versions and... generally... adheres to RFC 4122. In this revision the API has been simplified and the version 2 UUID implementation has been removed. All UUID generation is done through a gen_server. Requests for V1 and V4 UUIDs can be batched. The last change is the ability to set two options: MAC privacy and UUID version utilized.
+This project implements most UUID versions and... generally... adheres to RFC 4122. In this revision the API has been simplified and the version 2 UUID implementation has been removed. All UUID generation is done through a gen_server. Requests UUIDs can be batched. The last change is the ability to set two options: MAC privacy and UUID version utilized.
 
-I favor simplicity over strict ahderence, a common theme in my projects. Due to how namespace UUIDs are generated, it is recommended to use this in new projects or a new instance of projects which hold UUIDs in persistent storage. 
+I favor simplicity over strict ahderence, a common theme in my projects. Due to how namespace UUIDs are generated, it is recommended to convert any pre-existing UUIDs in persistent storage. 
 
 #### General Behavior / Assumptions:
  - One instance running over complete system; used in one EVM.
@@ -37,13 +37,18 @@ Options:
 API:
 
  - nil/0: Returns <<0:128>>
- - new/0: Returns one V1 or V4 UUID in a list; gen_server state must indicate v1 or v4.
- - new/1: Returns _n_ V1 or V4 UUIDs in a list; (as above).
- - new/2: Returns a V3 or V5 namespace UUID; gen_server state must indicate v3 or v5.
+ - new/0: Returns one V1 or V4 UUID; gen_server state must indicate v1 or v4.
+ - new/1: (Tuple) Returns a single V3 or V5 UUID.
+ - new/1: (List) Returns _n_ V3 or V5 UUIDs in a list.
+ - new/1: (Integer) Returns _n_ V1 or V4 UUIDs in a list.
+
  - setopt/1: Set MAC privacy for subsequent UUID generation; sets version for UUID generation.
  - to_string/1: Returns list representation of binary UUID in proper text format.
  - to_binary/1: Returns binary representation of text (as list) formatted UUID.
- - sync/0: Indicates to gen_server to recompute time offset used in time-based UUIDs.
+ - calibrate/0: Indicates to gen_server to recompute time offset used in time-based UUIDs.
+
+ - get_utc/1: Get creation time of V1 UUIDs in nanoseconds.
+ - get_utc/2: Specify time interval (second, milli..., micro..., nano...)
   
 ### References
 
--- src/uuid.erl
+++ src/uuid.erl
@@ -9,13 +9,18 @@
 
 -compile(inline).
 
+	%% 141427 		*	86400	*	1000000000
+	%% 1582 - Epoch		Sec/Day		NanoS/Sec
+-define(OFFSET_1582, 12219292800000000000).
+
 %% Build internal "unused" functions
 -export([v1/3, v4/0]).
 
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([nil/0, new/0, new/1, new/2, to_string/1, to_binary/1, sync/0, setopt/1]).
+-export([nil/0, new/0, new/1, to_string/1, to_binary/1]).
+-export([get_utc/1, get_utc/2, calibrate/0, setopt/1]).
 
 nil() -> <<0:128>>.
 
@@ -24,11 +29,10 @@ new() ->
 
 new(N) when is_integer(N) andalso N > 0 ->
 	gen_server:call(?MODULE, {new_uuid, N});
+new(N) when is_list(N) orelse is_tuple(N) > 0 ->
+	gen_server:call(?MODULE, {new_uuid, N});
 new(_) ->
 	{error, badarg}.
-
-new(NS, N) ->
-	gen_server:call(?MODULE, {new_uuid,NS,N}).
 
 to_string(<<_:128>> = UUID) ->
 	<<Int:128/integer-unsigned>> = UUID,
@@ -56,15 +60,32 @@ to_binary(String) ->
 		_:_ -> {error, badarg}
 	end.
 
-sync() -> gen_server:cast(?MODULE, calc_offset).
+calibrate() -> gen_server:cast(?MODULE, calc_offset).
 
-setopt(Param = {version, V}) when is_integer(V) andalso V >= 1 andalso V =< 5 ->
-	gen_server:call(?MODULE, Param);
-setopt(Param = {privacy, V}) when is_boolean(V) ->
-	gen_server:cast(?MODULE, Param);
+setopt({version, V}) when is_integer(V) andalso V >= 1 andalso V =< 5 ->
+	gen_server:call(?MODULE, {opt_version, V});
+setopt({privacy, V}) when is_boolean(V) ->
+	gen_server:cast(?MODULE, {opt_privacy, V});
 setopt(_) ->
 	{error, badarg}.
+
+get_utc(UUID) -> get_utc(UUID,nanosecond).
+
+get_utc(<<TL:4/binary, TM:2/binary, 2#0001:4, TH:12/bits, _:8/binary>>, Time) ->
+	<<I:64/integer-unsigned>> = <<0:4, TH:12/bits, TM:2/binary, TL:4/binary>>,
 	
+	case Time of
+		nanosecond -> (I * 100 -?OFFSET_1582); 
+		microsecond -> (I * 100 -?OFFSET_1582) div 1000; 
+		millisecond -> (I * 100 -?OFFSET_1582) div 1000000; 
+		second ->  (I * 100 -?OFFSET_1582) div 1000000000;
+		seconds ->  (I * 100 -?OFFSET_1582) div 1000000000;
+		_ ->	{error, badarg}
+	end;
+get_utc(_,_) ->
+	{error, badarg}.
+	  
+
 %% ====================================================================
 %% Behavioural functions
 %% ====================================================================
@@ -101,18 +122,18 @@ init(OptList) when is_list(OptList) ->
 %% handle_call/3
 %% ====================================================================
 handle_call({new_uuid, N}, _, {v4,_,_,_} = State) ->
-	{reply, iterate({v4,[]},[],N), State};
+	{reply, iterateT({v4,[]},[],N), State};
 
 handle_call({new_uuid, N}, _, State = {v1, Node, ClkSeq, Offset}) ->
-	{reply, iterate({v1,[Node, ClkSeq, Offset]}, [],N), State};
+	{reply, iterateT({v1,[Node, ClkSeq, Offset]}, [],N), State};
 
-handle_call({new_uuid, NS, N}, _, {v3,_,_,_} = State) ->
-	{reply, vNS(NS,N,3), State};
+handle_call({new_uuid, A}, _, {v3,_,_,_} = State) ->
+	{reply, iterateNS(3, [], A), State};
 
-handle_call({new_uuid, NS, N}, _, {v5,_,_,_} = State) ->
-	{reply, vNS(NS,N,5), State};
+handle_call({new_uuid, A}, _, {v5,_,_,_} = State) ->
+	{reply, iterateNS(5, [], A), State};
 
-handle_call({version, N}, _, {Version, Node, ClkSeq, Offset}) ->
+handle_call({opt_version, N}, _, {Version, Node, ClkSeq, Offset}) ->
 	{Reply, NewVer} =
 			try
 				{ok, list_to_existing_atom([$v|integer_to_list(N)])}
@@ -130,7 +151,7 @@ handle_call(_, _, State) ->
 %% ====================================================================
 handle_cast(calc_offset, {Version, Node, ClkSeq, _}) ->
 	{noreply, {Version, Node, ClkSeq, time_offset()}};
-handle_cast({privacy, V}, {Version, _, ClkSeq, Offset}) ->
+handle_cast({opt_privacy, V}, {Version, _, ClkSeq, Offset}) ->
 	{noreply, {Version, get_node(V), ClkSeq, Offset}};
 handle_cast(_, State) ->
     {noreply, State}.
@@ -152,11 +173,19 @@ code_change(_, State, _) ->
 %% Internal functions
 %% ====================================================================
 
-%% Loop
-iterate(_, Acc, 0) -> 
-	Acc;
-iterate(MFA = {F, A}, Acc, N) ->
-	iterate(MFA, [erlang:apply(?MODULE, F,A) | Acc], N-1).
+%% Loops
+iterateT(_, Acc, 0) -> 
+	case Acc of
+		[_] -> hd(Acc);
+		_ -> Acc
+	end;
+iterateT(MFA = {F, A}, Acc, N) ->
+	iterateT(MFA, [erlang:apply(?MODULE, F,A) | Acc], N-1).
+
+iterateNS(Ver, _, {NS,N}) ->	vNS(NS,N,Ver);
+iterateNS(_, Acc, []) ->		lists:reverse(Acc);
+iterateNS(Ver, Out, [{NS,N}|T]) ->
+	iterateNS(Ver, [vNS(NS,N,Ver) | Out], T).
 
 %% Node Stuff
 get_node(Private) ->
@@ -185,9 +214,7 @@ random_nodeid() ->
 
 %% Time Stuff
 time_offset() ->
-	%% 141427 		*	86400	*	1000000000
-	%% 1582 - Epoch		Sec/Day		NanoS/Sec
-	12219292800000000000 + erlang:time_offset(nanosecond).
+	?OFFSET_1582 + erlang:time_offset(nanosecond).
 
 timestamp(Offset) ->
 	(erlang:monotonic_time(nanosecond) + Offset) div 100.
